b6723d5ef9d8e335e862dd61f7db96063fdcbf18
zhangjiarui10 <1624301436@qq.com>
2020-07-07 15:18:30 +0800

    测试git

--- src/com/zhangjiarui/struts/login_judge.java
+++ src/com/zhangjiarui/struts/login_judge.java
@@ -3,7 +3,9 @@ package com.zhangjiarui.struts;
 import com.opensymphony.xwork2.ActionSupport;
 
 public class login_judge extends ActionSupport {
-
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
     @Override
     public String execute() throws Exception {
         return "success";
